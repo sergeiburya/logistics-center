60d5b93140d5e516b464aa7c80d215fa6ded497b
Serhii Buria <sergeiburya@gmail.com>
2023-02-09 14:10:17 +0200

    initial

---
+++ src/main/java/com/example/logisticscenter/LogisticsCenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.logisticscenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/logisticscenter/LogisticsCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logisticscenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
